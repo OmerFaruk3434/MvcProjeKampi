644f0dae6a94fade8cfd70dfbef779e0dd83bb27
Ömer Faruk Baysal <omerfarukbaysal3434@gmail.com>
2024-05-13 11:13:06 +0300

    60. lesson is over

--- DataAccessLayer/Abstract/IContactDal.cs
+++ DataAccessLayer/Abstract/IContactDal.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-	public interface IContactDal:IRepository<Content>
+	public interface IContactDal : IRepository<Contact>
 	{
+
 	}
 }
